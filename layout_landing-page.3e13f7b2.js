17906cb283ee306326cfbcf7986f5b3f6e14ca1c
Ievgen Petryk <jakeshad123@gmail.com>
2025-10-19 20:39:42 +0300

    make build

---
+++ layout_landing-page.3e13f7b2.js
@@ -0,0 +1,2 @@
+"use strict";var form=document.getElementById("contactForm");form.addEventListener("submit",function(t){form.checkValidity()&&(t.preventDefault(),form.reset())});
+//# sourceMappingURL=layout_landing-page.3e13f7b2.js.map
